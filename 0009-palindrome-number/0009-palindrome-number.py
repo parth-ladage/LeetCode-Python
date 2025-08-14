f0e6f63896dd23e8b3155e50e6b85ea99bac7f96
Parth Ladage <ladage.parth.p@gmail.com>
2025-08-14 17:36:40 +0530

    Time: 24 ms (10.88%), Space: 12.3 MB (81.46%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x<0 : return False
+        temp = x
+        rev=0
+        while x!=0:
+            rev = rev*10 + x%10
+            x = x//10
+        
+        return rev==temp
