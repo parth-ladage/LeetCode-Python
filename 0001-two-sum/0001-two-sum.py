ef0e2c4b42628551c3f304f8f28bdf3b46944e1e
Parth Ladage <ladage.parth.p@gmail.com>
2026-01-23 06:54:55 +0530

    Time: 0 ms (100%), Space: 20.7 MB (6.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,17 +1,16 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # n = len(nums)
+        # for i in range(n):
+        #     for j in range(1,n):
+        #         if nums[i]+nums[j] == target :
+        #             return [i,j]
         seen = {}
-
         for index, num in enumerate(nums):
             comp = target - num
             if comp in seen:
                 return [seen[comp], index]
-            seen[num]=index
-            
+            else:
+                seen[num] = index
         return []
         
