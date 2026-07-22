14bab3963a184ad68de3d7557c33ce39379c8c68
Parth Ladage <ladage.parth.p@gmail.com>
2026-07-22 12:13:42 +0530

    Time: 0 ms (100%), Space: 42.3 MB (15.86%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,6 +1,15 @@
 class Solution {
     public int fib(int n) {
-        if (n<=1) return n;
-        return fib(n-1) + fib(n-2);
+        // if (n<=1) return n;
+        // return fib(n-1) + fib(n-2);
+
+        int first = 0;
+        int second = 1;
+        for ( int i=1; i<=n ; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        return first;
     }
 }
